4f157a8e07a25b6a593fb42e95befe59fbf2d827
yONg <ivere27@gmail.com>
2016-04-19 23:31:20 +0900

    bug fix. 'archive not exists.' error was not raised on SCHEDUELR:/job/new

--- routes/schedulerJob.js
+++ routes/schedulerJob.js
@@ -2,7 +2,6 @@
 
 var debug = require('debug')('secc:routes:schedulerJob');
 var utils = require('../lib/utils');
-var path = require('path');
 
 module.exports = function(express, io, SECC, SCHEDULER) {
   var router = express.Router();
@@ -71,7 +70,7 @@ module.exports = function(express, io, SECC, SCHEDULER) {
     var archiveInfo = am.getArchiveInfo(am.getArchiveId(information));
     var crossArchiveIds = am.getArchiveIdsByTarget(information.dumpmachine);
 
-    if ((archiveInfo === null) && (job.crossPrefered && (crossArchiveIds.length === 0))) {
+    if ((archiveInfo === null) || (job.crossPrefered && (crossArchiveIds.length === 0))) {
       console.log('archive not exists. build local.')
       debug(information);
       jm.removeJob(job.id);
