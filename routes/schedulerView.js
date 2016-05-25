66f6d998756e13c519c87d07a5a44a7609ffe3ba
yONg <ivere27@gmail.com>
2016-05-25 23:02:23 +0900

    [scheduler] implement '/view' router.
    redirect to '/view/star' on '/' or '/view'

--- routes/schedulerView.js
+++ routes/schedulerView.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var debug = require('debug')('secc:routes:schedulerIndex');
+var debug = require('debug')('secc:routes:schedulerView');
 
 module.exports = function(express, io, SECC, SCHEDULER) {
   var router = express.Router();
@@ -10,7 +10,11 @@ module.exports = function(express, io, SECC, SCHEDULER) {
   var dm = SCHEDULER.dm;
 
   router.get('/', function(req, res) {
-    res.redirect('/view');
+    res.redirect('/view/star');
+  });
+
+  router.get('/star', function(req, res) {
+    res.render('schedulerViewStar');
   });
 
   return router;
